629b7e598ec41d5fc81e459a89572e7b42d3af68
sharanjaasenthurvelautham <sharusenthu@gmail.com>
2023-02-18 02:38:32 +0530

    fix: update url

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://main.d2sidjss97a7e1.amplifyapp.com/"
 };
 
 app.use(cors(corsOptions));
