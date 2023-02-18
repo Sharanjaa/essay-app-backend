0300fffcc4ff156fdfe3482758d81aac147441c0
sharanjaasenthurvelautham <sharusenthu@gmail.com>
2023-02-18 06:27:35 +0530

    fix: cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
+var corsOptions = {
+  origin: ["https://www.elanguagecenter.com/", "https://www.elanguagecenter.com", "http://localhost:8081/","http://localhost:8081"],
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
