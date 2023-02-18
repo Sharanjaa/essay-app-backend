2a078f10aeefe99354c4258e656f7863bf8920bb
sharanjaasenthurvelautham <sharusenthu@gmail.com>
2023-02-18 10:48:27 +0530

    feat:token

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: "bezkoder-secret-key",
-  OPENAI_API_KEY: "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",
+  OPENAI_API_KEY: "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",
   VITE_CLIENT_ID: "AfS1sGSXf6nVbVW8u5khkb3KHAk-6b-vJ8Yp6inwdgDkpqtn9d1U3iU1dLVyZgSWtn24kLrjdpezwLZe",
   REACT_APP_VITE_CLIENT_ID: "AfS1sGSXf6nVbVW8u5khkb3KHAk-6b-vJ8Yp6inwdgDkpqtn9d1U3iU1dLVyZgSWtn24kLrjdpezwLZe"
 };
