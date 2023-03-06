a830821e11ec1de20079305f0f10429e47c14d40
sharanjaasenthurvelautham <sharusenthu@gmail.com>
2023-03-06 16:37:38 +0530

    featy: update value

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: "bezkoder-secret-key",
-  OPENAI_API_KEY: "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",
+  OPENAI_API_KEY: "sk-wrwt",
   VITE_CLIENT_ID: "AfS1sGSXf6nVbVW8u5khkb3KHAk-6b-vJ8Yp6inwdgDkpqtn9d1U3iU1dLVyZgSWtn24kLrjdpezwLZe",
   REACT_APP_VITE_CLIENT_ID: "AfS1sGSXf6nVbVW8u5khkb3KHAk-6b-vJ8Yp6inwdgDkpqtn9d1U3iU1dLVyZgSWtn24kLrjdpezwLZe"
 };
